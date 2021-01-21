9ad0c7b0bef8b1bc3660c8444e0371debc4ae30c
“Landon <landonwjohnson@gmail.com>
2021-01-21 09:42:52 -0700

    in process of creating seeder for question scschema

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-
+    protected $fillable = ['title', 'body'];
     public function user(){
         return $this->belongsTo(User::class);
     }
 
-    $question = Question::find(1)
+    // $question = Question::find(1);
 }
 
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        factory(App\User::class, 3)->create().each(function($u){
+            $u->questions()
+                -> saveMany(
+                    factory(App\Question::class, rand(1, 5))->make()
+                )
+        });
     }
 }
